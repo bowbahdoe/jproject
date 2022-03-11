f889341bc898b3f746cee75bb4de982d681302b3
Ethan McCue <ethan@mccue.dev>
2022-03-10 20:50:01 -0500

    Remove garbage

--- example/modules/dev.mccue.example/src/A.java
+++
@@ -1,7 +0,0 @@
-import dev.mccue.magicbean.MagicBean;
-
-@MagicBean
-final class A extends ABeanOps {
-    int x;
-    String aaa;
-}
--- example/modules/dev.mccue.example/src/Main.java
+++
@@ -1,7 +0,0 @@
-import org.apache.commons.text.WordUtils;
-
-public final class Main {
-    public static void main(String[] args) {
-        System.out.println(WordUtils.capitalize("hello world"));
-    }
-}
